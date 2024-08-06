f70bd06832273927cb8a5224a420dccedef48582
Jishnu M M <jishnumm4312@gmail.com>
2024-08-07 00:02:24 +0530

    added custom logo

--- client/main.js
+++ client/main.js
@@ -1,7 +1,7 @@
 const { app, BrowserWindow } = require('electron');
 const path = require('path');
 
-function createWindow () {
+function createWindow() {
   const mainWindow = new BrowserWindow({
     width: 1200,
     height: 800,
@@ -9,7 +9,8 @@ function createWindow () {
       preload: path.join(__dirname, 'preload.js'),
       nodeIntegration: true,
       contextIsolation: false
-    }
+    },
+    icon: path.join(__dirname, 'assets', 'icon.png') // Path to your icon file
   });
 
   mainWindow.loadFile('src/index.html');
